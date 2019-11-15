e8fdf904ce56bc1fd653e76bc431dc36bb32456d
yakov.burtsev <yakov.burtsev@revolut.com>
2019-11-15 19:48:25 +0300

    @JsonValue

---
+++ src/main/java/com/yaburtsev/jackson/demo/annotations/writeonly/JsonValueDemo.java
@@ -0,0 +1,51 @@
+package com.yaburtsev.jackson.demo.annotations.writeonly;
+
+import com.fasterxml.jackson.annotation.JsonValue;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+import static java.lang.System.out;
+
+public class JsonValueDemo {
+
+    public static void main(String[] args) throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+
+        Props1 p1 = new Props1();
+        mapper.writeValue(new File("props1.json"), p1);
+
+        Props2 p2 = new Props2();
+        mapper.writeValue(new File("props2.json"), p2);
+
+        out.println("serialization successful");
+    }
+
+    private static class Props1 {
+
+        public String a = "A";
+        public String b = "B";
+
+        @JsonValue
+        public String toJSON() {
+            return a + "\"-\"" + b;
+        }
+    }
+
+    private static class Props2 {
+
+        private Map<String, String> props = new HashMap<>();
+
+        Props2() {
+            props.put("a", "A'A'\"A\"");
+            props.put("b", "B'B'\"B\"");
+        }
+
+        @JsonValue
+        public Map<String, String> toJSON() {
+            return props;
+        }
+    }
+}
